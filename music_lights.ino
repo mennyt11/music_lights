0e6d018db1b1e1e97f3e9ab64bb8c61dccddeb6b
Menny Thankachan <31594856+mennyt11@users.noreply.github.com>
2021-12-31 15:28:38 +0530

    Create music_lights.ino

---
+++ music_lights.ino
@@ -0,0 +1,708 @@
+void init_seq()
+{
+        delay(500);
+        digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,HIGH) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;       
+      
+      
+      delay(500);
+      
+        digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;
+        digitalWrite(4,HIGH);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+      
+      
+      delay(500);
+      
+        digitalWrite(8,LOW);
+        digitalWrite(7,HIGH) ;
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,LOW);
+
+        delay(500);
+        
+        digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH) ;
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH);
+
+        delay(500);
+        
+        digitalWrite(8,LOW);
+        digitalWrite(7,HIGH) ;
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,LOW);
+
+        
+        delay(500);
+        
+        digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;
+        digitalWrite(4,HIGH);
+        digitalWrite(3,LOW);
+        digitalWrite(2,HIGH);
+
+      
+      delay(500);
+      
+        digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,HIGH) ;
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+        
+        delay(500);
+        
+        digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+
+}
+
+void seq0()
+{
+  delay(100);
+        digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;       
+      
+  for (int i=0;i<5;i++)
+  {
+        
+      
+      delay(100);
+      digitalWrite(8,HIGH);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+      
+      
+      delay(100);
+      digitalWrite(8,LOW);
+        digitalWrite(7,HIGH) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+
+        delay(100);
+        digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,HIGH);
+        digitalWrite(5,LOW) ;
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+delay(100);
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,HIGH) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;
+        
+delay(100);
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,HIGH);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;
+
+delay(100);
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,LOW) ;
+
+
+delay(100);
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,HIGH) ;
+
+delay(100);
+
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,LOW) ;
+
+        
+delay(100);
+
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,HIGH);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;
+
+delay(100);
+
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,HIGH) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;
+
+delay(100);
+
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,HIGH);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;
+
+               
+delay(100);
+
+digitalWrite(8,LOW);
+        digitalWrite(7,HIGH) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;
+
+        
+delay(100);
+
+digitalWrite(8,HIGH);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;
+  }
+}
+
+
+void seq1()
+{
+  delay(100);
+  
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;       
+      
+  for (int i=0;i<5;i++)
+  {
+        
+      
+      delay(100);
+      
+digitalWrite(8,HIGH);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,HIGH);
+        digitalWrite(5,LOW) ;
+        digitalWrite(4,HIGH);
+        digitalWrite(3,LOW);
+        digitalWrite(2,HIGH);
+      
+      
+      delay(100);
+      
+digitalWrite(8,LOW);
+        digitalWrite(7,HIGH) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,HIGH) ;
+        digitalWrite(4,LOW);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,LOW);             
+  }
+}
+
+
+void seq2()
+{
+  delay(100);
+  
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;       
+      
+  for (int i=0;i<5;i++)
+  {
+delay(100);        
+digitalWrite(8,HIGH);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;       
+
+      
+delay(100);        
+digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;
+        
+      delay(100);
+digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH) ;
+      
+        digitalWrite(6,HIGH);
+        digitalWrite(5,LOW) ;
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+      
+      
+      delay(100);
+digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH) ;
+
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+
+        delay(100);
+digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH) ;
+
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;
+        digitalWrite(4,HIGH);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+delay(100);
+digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH) ;
+
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;      
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,LOW) ;
+        
+delay(100);
+digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH) ;
+
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;      
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH) ;
+
+delay(100);
+digitalWrite(8,LOW);
+        digitalWrite(7,HIGH) ;
+
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;      
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH) ;
+
+
+delay(100);
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;      
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH) ;
+
+delay(100);
+
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(5,HIGH) ;      
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH); 
+
+
+delay(100);
+        
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH);
+
+        
+delay(100);
+
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH);
+
+        
+delay(100);
+
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,HIGH);
+
+        
+delay(100);
+
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+               
+  }
+}
+
+void seq3()
+{
+  delay(100);
+  
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;       
+      
+  for (int i=0;i<10;i++)
+  {
+        
+      
+        
+delay(100);
+
+digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH);
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;      
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH) ;
+
+
+        
+delay(100);
+
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+               
+  }
+}
+
+
+void seq4()
+{
+  delay(100);
+  
+digitalWrite(8,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;       
+      
+  for (int i=0;i<5;i++)
+  {
+        
+      delay(100);
+  digitalWrite(8,HIGH);
+        digitalWrite(7,LOW);      
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,HIGH);
+     
+      delay(100);
+  digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH);      
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;
+        digitalWrite(4,LOW);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH);
+      
+       delay(100);
+  digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH);      
+        digitalWrite(6,HIGH);
+        digitalWrite(5,LOW) ;
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH);
+     
+        delay(100);
+  digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH);      
+ 
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH);
+
+     delay(100);
+  digitalWrite(8,LOW);
+        digitalWrite(7,HIGH);      
+ 
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,LOW);
+ 
+delay(100);
+
+  digitalWrite(8,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH) ;      
+        digitalWrite(4,HIGH);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;
+
+delay(100);
+
+  digitalWrite(8,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(5,HIGH) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW); 
+
+
+delay(100);
+
+  digitalWrite(8,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW) ;      
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW);
+
+               
+  }
+}
+
+void music_lights()
+{
+  int val;
+      val=analogRead(A2);   //connect mic sensor to Analog 0\2
+      Serial.print("Sound=");
+      Serial.println(val);//print the sound value to serial
+      if(val<675)
+      {
+        
+        digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH) ;
+        
+        digitalWrite(6,HIGH);
+        digitalWrite(5,HIGH);
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH) ;       
+
+        
+      }
+      
+      else if(val<700)
+      {
+        
+        digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH) ;
+        digitalWrite(6,HIGH);
+        digitalWrite(5,LOW);
+        digitalWrite(4,HIGH);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH) ;       
+      }
+      else if(val<720)
+      {
+        
+        digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(3,HIGH);
+        digitalWrite(2,HIGH) ;       
+      }
+      
+      else if(val<740)
+      {
+        
+        digitalWrite(8,HIGH);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,HIGH) ;       
+      }
+      
+      else if(val>740)
+      {
+        
+        digitalWrite(8,LOW);
+        digitalWrite(7,LOW) ;
+        digitalWrite(6,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(2,LOW) ;       
+      }
+      
+      
+delay(100);
+/*
+digitalWrite(8,HIGH);
+digitalWrite(7,HIGH) ;
+digitalWrite(6,HIGH);
+digitalWrite(5,HIGH) ;
+digitalWrite(4,HIGH);
+digitalWrite(3,HIGH);
+digitalWrite(2,HIGH) ;       
+*/
+}
+
+void sequence_gen(int randnum=0)
+{
+if(randnum==0)
+{
+  seq0();  
+}
+else if(randnum ==1)
+{
+  seq1();
+}
+else if(randnum==2)
+{
+  seq2();
+}
+else if(randnum==3)
+{
+  seq3();
+}
+else
+{
+  seq4();
+}
+}
+
+void setup(){ 
+  Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
+      pinMode(8, OUTPUT);
+      pinMode(7, OUTPUT);
+      pinMode(6, OUTPUT);
+      pinMode(5, OUTPUT);
+      pinMode(4, OUTPUT);
+      pinMode(3, OUTPUT);
+      pinMode(2, OUTPUT);
+     
+
+      pinMode(13, INPUT);
+      
+      init_seq();
+}
+
+void loop(){
+  int randnumber;
+  int switch_val;
+  switch_val=digitalRead(13);
+  if(switch_val==HIGH)
+  {
+  music_lights();      
+  }
+  else
+  {
+    randnumber=random(5);
+    sequence_gen(randnumber);
+  }
+}
